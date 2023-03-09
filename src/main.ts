f472a768ecbf24d8a292fa9f923d7d0950aa3582
Víctor Várez <kaliayev@protonmail.com>
2023-03-09 18:20:00 +0100

    post request

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,14 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { ValidationPipe } from '@nestjs/common';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  app.useGlobalPipes(
+    new ValidationPipe({
+      whitelist: true, // this is a security thing in case of requests that have more fields than expected
+    }),
+  );
   await app.listen(3000);
 }
 bootstrap();
